b0f3171972607a0ebd39e53253ff35e41ab7968a
Geon Lee <leegun2003@gmail.com>
2019-10-27 23:38:59 +0900

    ✨Update simple example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GlobalStyle } from "./global-styles";
+import styled from "styled-components";
 
-const App: React.FC = () => {
+// This is example of styled-components
+const Container = styled.main`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+// This is example of styled-components with TypeScript or you can make interface of props
+const Header = styled("header")<{ toggleState: boolean }>`
+  background: linear-gradient(#eee, #333);
+  -webkit-background-clip: text;
+  color: ${({ toggleState }) => (toggleState ? "grey" : "black")};
+  transition: 0.5s ease-in-out;
+  font-size: 3rem;
+  font-weight: 900;
+  text-transform: uppercase;
+`;
+
+// This is example of react-hooks with TypeScript
+function useToggle(defaultValue: boolean) {
+  const [toggleState, setToggleState] = useState(defaultValue);
+
+  function onMouseOver() {
+    setToggleState(true);
+  }
+
+  function onMouseOut() {
+    setToggleState(false);
+  }
+
+  return { toggleState, onMouseOver, onMouseOut };
+}
+
+function App() {
+  const toggle = useToggle(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Container className="App">
+        <Header {...toggle} className="App-header">
+          react, typescript, styled-components
+          <br />
+          boilerplate
+        </Header>
+      </Container>
+    </>
   );
 }
 

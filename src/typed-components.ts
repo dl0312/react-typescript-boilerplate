e24b29eec08a0eaa951d96cba84dedbf2b70bb2b
Geon Lee <leegun2003@gmail.com>
2019-10-27 23:37:27 +0900

    🔧 Add typed-components for global-style type

---
+++ src/typed-components.ts
@@ -0,0 +1,17 @@
+import * as styledComponents from "styled-components";
+import { ThemedStyledComponentsModule } from "styled-components";
+
+interface IThemeInterface {
+  injectGlobal: string;
+}
+
+const {
+  default: styled,
+  css,
+  createGlobalStyle,
+  keyframes,
+  ThemeProvider
+} = styledComponents as ThemedStyledComponentsModule<IThemeInterface>;
+
+export { css, createGlobalStyle, keyframes, ThemeProvider };
+export default styled;

92d4ba3b110666462f4eaffa580b58af54a7a1d0
Geon Lee <leegun2003@gmail.com>
2019-10-30 12:28:20 +0900

    📄Add escription

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -1,11 +1,28 @@
-import { createGlobalStyle } from "./typed-components";
-import { fontSize, color, media } from "./config/_mixin";
+import { createGlobalStyle } from './typed-components';
+import { fontSize, color, media } from './config/_mixin';
 export const GlobalStyle = createGlobalStyle`
+  /*
+    normalize.css - https://necolas.github.io/normalize.css/
+  */
   @import url("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css");
+  /*
+    Fontawesome - https://fontawesome.com/
+  */
   @import url("https://use.fontawesome.com/releases/v5.11.2/css/all.css");
-  /* 나눔 고딕체, 도현체, 나눔 명조체, 나눔 펜체 */
-  @import url("https://fonts.googleapis.com/css?family=Nanum+Gothic|Playfair+Display|Do+Hyeon|Song+Myung|Thasadith|Nanum+Myeongjo|Nanum+Pen+Script");
-  
+  /*
+    Web Fonts from Google Fonts
+
+    배달의민족 도현 - https://fonts.google.com/specimen/Do+Hyeon
+    송명 - https://fonts.google.com/specimen/Song+Myung
+    나눔 고딕 - https://fonts.google.com/specimen/Nanum+Gothic
+    나눔 명조 - https://fonts.google.com/specimen/Nanum+Myeongjo
+    나눔손글씨 펜 - https://fonts.google.com/specimen/Nanum+Pen+Script
+    검은고딕 - https://fonts.google.com/specimen/Black+Han+Sans
+
+    Use the following CSS rules to specify these families:
+    font-family: 'Black Han Sans', sans-serif;
+  */
+  @import url("https://fonts.googleapis.com/css?family=Nanum+Gothic|Black+Han+Sans|Do+Hyeon|Song+Myung|Nanum+Myeongjo|Nanum+Pen+Script");
   * {
       box-sizing: border-box;
   }
@@ -13,8 +30,10 @@ export const GlobalStyle = createGlobalStyle`
   body{
     font-family: 'Nanum Gothic', sans-serif;
     font-size: ${fontSize.normalFontSize};
-    background-color: ${color.bgColor};
-    color: ${color.fontColor};
+    /* background-color: ${color.default.bgColor};
+    color: ${color.default.fontColor}; */
+    background-color: ${color.darkmode.bgColor};
+    color: ${color.darkmode.fontColor};
     ${media.giant} {
       /* Giant View */
     }
@@ -32,7 +51,8 @@ export const GlobalStyle = createGlobalStyle`
   a{
     text-decoration: none !important;
     &:hover{
-        color: ${color.mainColor};
+        color: ${color.default.mainColor};
+        color: ${color.darkmode.mainColor};
     }
   }
 

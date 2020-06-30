7e6968e578d347b711cfeba42b7c58b91e20bc23
geon lee <geon.lee@navercorp.com>
2020-06-30 15:28:08 +0900

    🐛 createGlobalStyle에서 @import로 가져오는 것 대신 index.html에서 link를 통해 cdn에서 가져오는 방식으로 수정

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -1,32 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import { fontSize, color, media } from './config/_mixin';
-import 'react-toastify/dist/ReactToastify.min.css';
 import { IThemeInterface } from './interfaces';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export const GlobalStyle = createGlobalStyle<IThemeInterface>`
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/antd/4.1.1/antd.min.css");
-  /*
-    normalize.css - https://necolas.github.io/normalize.css/
-  */
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css");
-  /*
-    Fontawesome - https://fontawesome.com/
-  */
-  @import url("https://use.fontawesome.com/releases/v5.11.2/css/all.css");
-  /*
-    Web Fonts from Google Fonts
-
-    배달의민족 도현 - https://fonts.google.com/specimen/Do+Hyeon
-    송명 - https://fonts.google.com/specimen/Song+Myung
-    나눔 고딕 - https://fonts.google.com/specimen/Nanum+Gothic
-    나눔 명조 - https://fonts.google.com/specimen/Nanum+Myeongjo
-    나눔손글씨 펜 - https://fonts.google.com/specimen/Nanum+Pen+Script
-    검은고딕 - https://fonts.google.com/specimen/Black+Han+Sans
-
-    Use the following CSS rules to specify these families:
-    font-family: 'Black Han Sans', sans-serif;
-  */
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
   *,
   *::after,
   *::before {
@@ -38,12 +15,10 @@ export const GlobalStyle = createGlobalStyle<IThemeInterface>`
     font-size: ${fontSize.normalFontSize};
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
     
-    transition: all 0.5s ease-in-out;
+    transition: background 0.5s ease-in-out;
+    transition: color 0.5s ease-in-out;
+
     /* Responsive Design */
     ${media.giant} {
       /* Giant View */

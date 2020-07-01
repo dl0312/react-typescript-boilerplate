b099136f305992417b578b1319ba85e7c90bd887
geon lee <geon.lee@navercorp.com>
2020-07-01 17:00:48 +0900

    ✨ useDarkMode as function export default

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -3,7 +3,7 @@ import { Theme } from '../interfaces';
 
 const THEME_KEY = 'theme';
 
-export const useDarkMode = (defaultTheme: Theme.LIGHT) => {
+export default function useDarkMode(defaultTheme: Theme.LIGHT) {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
   const toggleTheme = () => {
     if (theme === Theme.LIGHT) {
@@ -23,4 +23,4 @@ export const useDarkMode = (defaultTheme: Theme.LIGHT) => {
   }, []);
 
   return { theme, toggleTheme };
-};
+}
